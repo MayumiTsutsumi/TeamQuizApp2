f5c5ec08acdb2b73b602b3c2b911241ebb605392
RyoShimamura2026 <uni202607_15@telework.to>
2026-09-16 13:31:29 +0900

    AnswerCheckerの作成

---
+++ TeamQuizApp2/AnswerChecker.cs
@@ -0,0 +1,10 @@
+﻿namespace TeamQuizApp
+{
+    public class AnswerChecker
+    {
+        public bool CheckAnswer(Question q, int selectedIndex)
+        {
+            return q.CorrectIndex == selectedIndex;
+        }
+    }
+}
