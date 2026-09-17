6378da79700cc1347c6aa9a904432060fc9a7f1d
YukikoAshizawa <uni202607_01@telework.to>
2026-09-17 10:41:33 +0900

    １０回解答したら賞を表示する機能を追加

--- TeamQuizApp2/ScoreManager.cs
+++ TeamQuizApp2/ScoreManager.cs
@@ -14,15 +14,52 @@
         public string GetResult()
         {
             if (TotalCount == 0) return "正解数: 0 / 0 （正答率 0.0%）";
-            return $"正解数: {CorrectCount} / {TotalCount} （正答率 {((double)CorrectCount / TotalCount * 100):F1}%）";
 
+            double correctanswerate = GetCorrectAnswerRate();
+            return $"正解数: {CorrectCount} / {TotalCount} （正答率 {correctanswerate:F1}%）";
+
+        }
+
+        public double GetCorrectAnswerRate()
+        {
+            return (double)CorrectCount / TotalCount * 100;
         }
 
         public void GetPrize()
         {
+            string msg = "";
+            double correctanswerate = GetCorrectAnswerRate();
+
             if (TotalCount == 10)
             {
-                MessageBox.Show("１０回解答しました！");
+                msg = "10回解答しました！\n";
+
+                //正解率：100 % の場合→プラチナ賞
+                if (correctanswerate >= 100)
+                {
+                    msg += "プラチナ賞です！";
+                }
+                //正解率：90 % 以上の場合→金賞
+                else if (correctanswerate >= 90)
+                {
+                    msg += "金賞です！";
+                }
+                //正解率：70 % 以上の場合→銀賞
+                else if (correctanswerate >= 70)
+                {
+                    msg += "銀賞です！";
+                }
+                //正解率：50 % 以上の場合→銅賞
+                else if (correctanswerate >= 50)
+                {
+                    msg += "銅賞です！";
+                }
+                //その他：参加賞
+                else
+                {
+                    msg += "参加賞です！";
+                }
+                MessageBox.Show(msg);
             }
         }
     }
